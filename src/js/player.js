83535293373609a557cabe3af623177699c82cdc
Tendai Nyandoro <tendai1@hotmail.com>
2021-06-13 17:52:35 +0200

    Add the player object variables

--- src/js/player.js
+++ src/js/player.js
@@ -1,3 +1,8 @@
-function Player () {
-  
+function Player (classType, health, mana, agility, speed,) {
+  this.classType = classType;
+  this.health = health;
+  this.mana = mana;
+  this.strength = strength;
+  this.agility = agility;
+  this.speed = speed;
 }
