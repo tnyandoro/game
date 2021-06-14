0fc81555e7d3e706741abae27c3d46000578d4cb
Tendai Nyandoro <tendai1@hotmail.com>
2021-06-14 10:53:15 +0200

    Add all the players onto the swicth statements

--- src/js/gamemanager.js
+++ src/js/gamemanager.js
@@ -10,10 +10,15 @@ const GameManager = {
       case 'Warrior':
         player = new Player(classType, 200, 0, 200, 100, 50);
         break;
-        case 'Rouge':
-        player = new Player(classType, 200, 0, 200, 100, 50);
+      case 'Rouge':
+        player = new Player(classType, 100, 0, 100, 150, 200);
         break;
-
+      case 'Mage':
+        player = new Player(classType, 80, 0, 50, 200, 50);
+        break;
+      case 'Hunter':
+        player = new Player(classType, 200, 0, 50, 200, 100);
+        break;
       default:
         break;
     }
